0ebcce5d4439ff48345e93eaf9e60de8290f1049
Vikash <Vik80sh@gmail.com>
2020-07-08 14:27:40 +0530

    first commit

---
+++ JavaScript.ts
@@ -0,0 +1,16 @@
+let baseSalary:number=3000;
+let roomRent:number = 2500;
+let groceries:number = 1500;
+
+function calculateSalary(baseSalary:number,roomRent:number,groceries:number) {
+    return baseSalary + roomRent + groceries;
+    
+}
+
+// object orianted stype
+
+const emplyeeSalar = {
+    baseSalary:3000,
+    roomRent:2500,
+    groceries:1500,  
+}
